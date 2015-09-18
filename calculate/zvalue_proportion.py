cbc51a135669ebb7bfc5c4c77f81ef1c2d86679f
Ryan Menezes <ryanvmenezes@gmail.com>
2015-09-17 23:30:36 -0700

    not sure if this is the right way to pass a function

--- calculate/zvalue_proportion.py
+++ calculate/zvalue_proportion.py
@@ -1,6 +1,15 @@
 import math
 from __future__ import division # to ensure integer division does not happen
 
+def mean_pct():
+	try:
+        numerator = list(map(float, numerator))
+        denominator = list(map(float, denominator))
+    except ValueError:
+        raise ValueError('Input values must contain numbers')
+    
+	return sum(numerator) / sum(denominator)
+
 def zval_prop(numerator, denominator, mean_pct = sum(numerator) / sum(denominator)):
 	'''
 	A calculation to determine how many standard errors (deviations)
@@ -16,13 +25,20 @@ def zval_prop(numerator, denominator, mean_pct = sum(numerator) / sum(denominato
 	
 
 	'''
+
+	# Make sure everything is a number
 	try:
         numerator = list(map(float, numerator))
         denominator = list(map(float, denominator))
     except ValueError:
         raise ValueError('Input values must contain numbers')
 
+    # Make sure lists are same length
     if len(numerator) != len(denominator):
-        raise ValueError('The two lists you provided do not have the same \
-number of entries. Pearson\'s r can only be calculated with paired data.')
+        raise ValueError('The two lists you provided do not have the same number of entries. Z-values can only be calculated with paired data.')
+
+    # Make list of percentages
+    percentages = [ n / d for n, d in zip(numerator, denominator)]
+
+
 	return [ (( n / d ) - mean_pct) / math.sqrt( mean_pct * (1 - mean_pct) /  d ) for n, d in zip(numerator, denominator) ]
